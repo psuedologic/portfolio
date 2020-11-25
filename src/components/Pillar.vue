17b95d5114375341afa5df25fb3ad47877c7c431
Talon Gonyeau <talongonyeau@gmail.com>
2020-11-24 17:49:08 -0800

    Add canvas and pillar components

---
+++ src/components/Pillar.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+import h  from 'vue'
+import Zdog from "zdog";
+// import {} from './ZdogCanvas.vue'
+
+export default {
+  name: 'Pillar',
+}
+
+</script>
+
+<style scoped>
+</style>
