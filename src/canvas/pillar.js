7928f1cbd9b67b6a0226604baeb7dfcbc69acd69
Talon Gonyeau <talongonyeau@gmail.com>
2020-11-27 18:52:43 -0800

    Animation prototype

--- src/canvas/pillar.js
+++ src/canvas/pillar.js
@@ -5,14 +5,16 @@ const HEIGHT = 300
 
 class Pillar {
     constructor(anchor, position, scale) {
+        this.shape
         this.anchor = anchor
         this.position = position
+        this.angle = {x: 0, y: 0, z: 0}
         this.scale = scale
         this.create()
         
     }
     create() {
-        new Zdog.Box({
+        this.shape = new Zdog.Box({
             addTo: this.anchor,
             width: WIDTH,
             height: HEIGHT,
@@ -23,10 +25,25 @@ class Pillar {
             rightFace: '#E62',
             topFace: '#ED0',
             bottomFace: '#636',
+            scale: this.scale,
             translate: { x: this.position.x,
                          y: this.position.y,
-                         z: this.position.z}
+                         z: this.position.z},
         });
+    }
+    translate(vector) {
+        console.log(vector)
+        this.shape.translate = 
+        { x: this.position.x + vector.x,
+            y: this.position.y + vector.y,
+            z: this.position.z + vector.z}
+    }
+    rotate(vector) {
+        console.log(vector)
+        this.shape.rotate = 
+        { x: this.angle.x + vector.x,
+            y: this.angle.y + vector.y,
+            z: this.angle.z + vector.z}
     }
 }
 
--- src/components/ZdogCanvas.vue
+++ src/components/ZdogCanvas.vue
@@ -22,28 +22,59 @@ export default {
     })
     this.canvas = illo
 
-    let anchor = new Zdog.Anchor({ addTo: illo })
-    let pillar = new Pillar(anchor, { x: -200, z: -100 }, 1.0)
-    let pillar2 = new Pillar(anchor, { x: 200, z: -100 }, 1.0)
-    let pillar3 = new Pillar(anchor, { x: 0, z: 200 }, 1.0)
+    this.anchor = new Zdog.Anchor({ addTo: illo })
+    let pillar = new Pillar(this.anchor, { x: -200, z: -100 }, 1.0)
+    let pillar2 = new Pillar(this.anchor, { x: 200, z: -100 }, 1.0)
+    let pillar3 = new Pillar(this.anchor, { x: 0, z: 200 }, 1.25)
+    this.pillars = [pillar, pillar2, pillar3]
+
 
     illo.updateRenderGraph()
-    // animate
-    function animate() {
-      illo.updateRenderGraph();
-      requestAnimationFrame( animate );
-    }
-    animate();
+    this.animate()
   },
   data() {
-      return {
-          canvas: {},
-      }
+    return {
+      canvas: {},
+      anchor: Zdog.Anchor,
+      angle: 0,
+      pillars: [],
+    }
   },
   computed: {
-      getCanvas() {
-          return this.canvas
+    getCanvas() {
+      return this.canvas
+    }
+  },
+  methods: {
+    animate: function() {
+      this.canvas.updateRenderGraph()
+      this.angle++
+      const ROTATION_AMOUNT = 4
+      if (this.angle == 360 * ROTATION_AMOUNT) this.angle = 0
+      
+      if (this.anchor) {
+        this.anchor.rotate = {x: 0, y: -this.angle/90, z: 0}
       }
+      
+      this.pillars.forEach((pillar) => {
+        pillar.rotate({x: 0, y: this.angle/90 * 1.1, z: 0})
+      })
+      this.canvas.updateRenderGraph()
+      
+      requestAnimationFrame( this.animate );
+    }
+  },
+  watch: {
+    angle: {
+      handler(newValue, oldValue) {
+        
+        if (newValue == 360)
+          newValue = 0
+        else if (newValue % 5 == 0)
+          console.log(newValue)
+      },
+      immediate: true
+    }
   }
 }
 
