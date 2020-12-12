6ab55b09aab5bc78cb96e29bfbc25f62480b1f26
Talon Gonyeau <talongonyeau@gmail.com>
2020-12-11 19:28:23 -0800

    Single curly done

--- src/components/ZdogCanvas.vue
+++ src/components/ZdogCanvas.vue
@@ -26,9 +26,9 @@ export default {
     this.createPillars(3) 
     // let icon = new Icon(this.anchor, this.angularOffset[0], "bookClosed", 1.0)
     // let icon2 = new Icon(this.anchor, this.angularOffset[1], "bookOpen", 1.0)
-    let icon = new Icon(this.anchor, this.angularOffset[1], "bookStacked", 4.0, 1.0)
+    let icon = new Icon(this.anchor, this.angularOffset[1], "bookStacked", 1, 1.0)
     // First tests
-    let icon2 = new Icon(this.anchor, this.angularOffset[2], "curlyBraces", 4, 0)
+    let icon2 = new Icon(this.anchor, this.angularOffset[2], "curlyBraces", 12, 0)
 
     illo.updateRenderGraph()
     this.animate()
